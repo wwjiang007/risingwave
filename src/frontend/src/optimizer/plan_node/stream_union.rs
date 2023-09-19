305317d53ad7f32f00be5887a3037b44fd13455a
Shanicky Chen <peng@risingwave-labs.com>
2023-09-19 16:35:26 +0800

    Code cleanup: Removal of unnecessary println! statements

--- src/frontend/src/optimizer/plan_node/stream_union.rs
+++ src/frontend/src/optimizer/plan_node/stream_union.rs
@@ -24,9 +24,7 @@ use super::stream::prelude::*;
 use super::stream::StreamPlanRef;
 use super::utils::{childless_record, watermark_pretty, Distill};
 use super::{generic, ExprRewritable, PlanRef};
-use crate::optimizer::plan_node::generic::GenericPlanNode;
 use crate::optimizer::plan_node::generic::{GenericPlanNode, Union};
-use crate::optimizer::plan_node::stream::StreamPlanRef;
 use crate::optimizer::plan_node::{PlanBase, PlanTreeNode, StreamNode};
 use crate::optimizer::property::Distribution;
 use crate::stream_fragmenter::BuildFragmentGraphState;
@@ -42,7 +40,6 @@ impl StreamUnion {
     pub fn new(logical: generic::Union<PlanRef>) -> Self {
         let inputs = &logical.inputs;
         let dist = inputs[0].distribution().clone();
-
         // assert!(inputs.iter().all(|input| *input.distribution() == dist));
         Self::new_with_dist(logical, dist)
     }
