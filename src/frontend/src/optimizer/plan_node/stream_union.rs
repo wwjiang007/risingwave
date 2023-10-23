3cbd3504a6147ef2baadfb91f3d7c145f0add347
Shanicky Chen <peng@risingwave-labs.com>
2023-10-23 14:56:00 +0800

    Remove `Union` import, add `GenericPlanNode` import.

--- src/frontend/src/optimizer/plan_node/stream_union.rs
+++ src/frontend/src/optimizer/plan_node/stream_union.rs
@@ -21,7 +21,7 @@ use risingwave_pb::stream_plan::UnionNode;
 
 use super::utils::{childless_record, watermark_pretty, Distill};
 use super::{generic, ExprRewritable, PlanRef};
-use crate::optimizer::plan_node::generic::{GenericPlanNode, Union};
+use crate::optimizer::plan_node::generic::GenericPlanNode;
 use crate::optimizer::plan_node::stream::StreamPlanRef;
 use crate::optimizer::plan_node::{PlanBase, PlanTreeNode, StreamNode};
 use crate::stream_fragmenter::BuildFragmentGraphState;
